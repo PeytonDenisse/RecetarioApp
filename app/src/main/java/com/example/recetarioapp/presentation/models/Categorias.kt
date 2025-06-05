5acb6dcb4ba2ec5a8b2b56ec624a0dd57a9ef104
Greys2004 <gav77465@lasallebajio.edu.mx>
2025-06-05 01:34:15 -0600

    Creacion de los modelos

---
+++ app/src/main/java/com/example/recetarioapp/presentation/models/Categorias.kt
@@ -0,0 +1,8 @@
+package com.example.recetarioapp.presentation.models
+
+data class Categorias(
+    val __v: Int,
+    val _id: String,
+    val category: String,
+    val image: String
+)
--- app/src/main/java/com/example/recetarioapp/presentation/models/Receta.kt
+++
@@ -1,4 +0,0 @@
-package com.example.recetarioapp.presentation.models
-
-class Receta {
-}
