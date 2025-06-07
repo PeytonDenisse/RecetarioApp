a0d5cf33939dc47ae21d6cd43cbfb01adb785c34
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-06-07 14:21:59 -0600

    pantalla de favoritos

--- app/src/main/java/com/example/recetarioapp/MainActivity.kt
+++ app/src/main/java/com/example/recetarioapp/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.*
+import com.example.recetarioapp.presentation.screens.FavoritesScreens
 import com.example.recetarioapp.presentation.screens.HomeScreen
 import com.example.recetarioapp.presentation.ui.theme.RecetarioAppTheme
 import com.example.recetarioapp.presentation.utils.Bookmark
@@ -108,8 +109,8 @@ class MainActivity : ComponentActivity() {
                         }
 
                         composable("favorites") {
-                            // Pantalla de favoritos
-                            Text("Pantalla Favoritos", modifier = Modifier.padding(16.dp))
+                            FavoritesScreens(innerPadding = innerPadding, navController = navController)
+                        }
                         }
 
 
@@ -118,7 +119,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+
+
 
 
 @Preview(showBackground = true)
