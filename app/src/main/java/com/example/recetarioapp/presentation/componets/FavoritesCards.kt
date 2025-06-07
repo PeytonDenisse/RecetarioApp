8217321f5c94db008867ffdcc252338da1e522e3
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-06-07 14:01:16 -0600

    card de recetas favoritas

--- app/src/main/java/com/example/recetarioapp/presentation/componets/FavoritesCards.kt
+++ app/src/main/java/com/example/recetarioapp/presentation/componets/FavoritesCards.kt
@@ -1,2 +1,104 @@
 package com.example.recetarioapp.presentation.componets
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.example.recetarioapp.presentation.models.Recipe
+
+@Composable
+fun FavoriteCard(
+    recipe: Recipe,
+    onActionClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Imagen de receta
+            AsyncImage(
+                model = recipe.imageUrl,
+                contentDescription = recipe.title,
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(12.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            // Info de la receta
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = recipe.title,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.Black,
+                    maxLines = 1
+                )
+                Text(
+                    text = recipe.category,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Gray,
+                    maxLines = 1
+                )
+
+            }
+
+            // Botón
+            Button(
+                onClick = onActionClick,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFC107)),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text("Ver receta", color = Color.Black)
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = false)
+@Composable
+fun PreviewFavoriteCard() {
+    FavoriteCard(
+        recipe = Recipe(
+            title = "Bear's wish",
+            category = "Infantil",
+            imageUrl = "https://cdn-icons-png.flaticon.com/512/1046/1046757.png",
+            rating = 4.5,
+            time = "15 min",
+            decription = "Un cuento para dormir con moraleja"
+        ),
+        onActionClick = { /* Navegar al detalle o eliminar */ }
+    )
+}
